67b3bc34eea6076bdeb699fabfa5b5366ba1817a
Max <korobprog@gmail.com>
2022-04-05 10:57:00 +1000

    calc function

--- src/lesson2/mathOperators.test.ts
+++ src/lesson2/mathOperators.test.ts
@@ -1,29 +1,47 @@
-import { mul, div, add, minus, exp } from "./mathOperators";
+import {
+  getValueFunctionMul,
+  getValueFunctionDiv,
+  getValueFunctionAdd,
+  getValueFunctionMinus,
+  getValueFunctionExponentiation,
+  getValueFunctionSquaring,
+  getValueFunctionFactorial,
+} from "./mathOperators";
 
-test("mul 1 * 2 to equal 2", () => {
-  expect(mul(1, 2)).toBe(2);
-});
+describe("mathOperators test cases", () => {
+  it("getValueFunctionMul 1 * 2 to equal 2", () => {
+    expect(getValueFunctionMul(1, 2)).toBe(2);
+  });
 
-test("mul 2 * 2 to equal 4", () => {
-  expect(mul(2, 2)).toBe(4);
-});
+  it("getValueFunctionMul 2 * 2 to equal 4", () => {
+    expect(getValueFunctionMul(2, 2)).toBe(4);
+  });
 
-test("div 2 / 2 to equal 1", () => {
-  expect(div(2, 2)).toBe(1);
-});
+  it("getValueFunctionDiv 2 / 2 to equal 1", () => {
+    expect(getValueFunctionDiv(2, 2)).toBe(1);
+  });
 
-test("div 4 / 2 to equal 2", () => {
-  expect(div(4, 2)).toBe(2);
-});
+  it("getValueFunctionDiv 4 / 2 to equal 2", () => {
+    expect(getValueFunctionDiv(4, 2)).toBe(2);
+  });
 
-test("add 4 + 2 to equal 6", () => {
-  expect(add(4, 2)).toBe(6);
-});
+  it("getValueFunctionAdd 4 + 2 to equal 6", () => {
+    expect(getValueFunctionAdd(4, 2)).toBe(6);
+  });
 
-test("minus 4 - 2 to equal 2", () => {
-  expect(minus(4, 2)).toBe(2);
-});
+  it("getValueFunctionMinus 4 - 2 to equal 2", () => {
+    expect(getValueFunctionMinus(4, 2)).toBe(2);
+  });
 
-test("sin 4 x 2 to equal 2", () => {
-  expect(minus(4, 2)).toBe(2);
+  it("getValueFunctionExponentiation 2 ^ 3 to equal 8", () => {
+    expect(getValueFunctionExponentiation(2, 3)).toBe(8);
+  });
+
+  it("getValueFunctionSquaring 5 ** to equal 25", () => {
+    expect(getValueFunctionSquaring(5)).toBe(25);
+  });
+
+  it("getValueFunctionFactorial ! 5 to equal 120", function () {
+    expect(getValueFunctionFactorial(5)).toBe(120);
+  });
 });
--- src/lesson2/runner.test.ts
+++ src/lesson2/runner.test.ts
@@ -1,29 +1,47 @@
 import { runner } from "./runner";
 
-test("runner: 1 * 32", () => {
-  expect(runner("1 * 32")).toEqual(32);
+describe("Runner simple cases", () => {
+  it("1 * 32", () => {
+    expect(runner("1 * 32")).toEqual(32);
+  });
+
+  it("2 * 32", () => {
+    expect(runner("2 * 32")).toEqual(64);
+  });
+
+  it("2 + 32", () => {
+    expect(runner("2 + 32")).toEqual(34);
+  });
+
+  it("7 ^ 2", () => {
+    expect(runner("7 ^ 2")).toEqual(49);
+  });
+
+  it("9 **", () => {
+    expect(runner("9 **")).toEqual(81);
+  });
 });
 
-test("runner: 2 * 32", () => {
-  expect(runner("2 * 32")).toEqual(64);
+describe("Runner tripled/mixed cases", () => {
+  it("2 * 2 * 3", () => {
+    expect(runner("2 * 2 * 3")).toEqual(12);
+  });
+
+  it("2 * 2 + 3", () => {
+    expect(runner("2 * 2 + 3")).toEqual(7);
+  });
+
+  it("2 + 2 * 3 ^ 2", () => {
+    expect(runner("2 + 2 * 3 ^ 2")).toEqual(38);
+  });
 });
 
-test("runner: 2 * 2 * 3", () => {
-  expect(runner("2 * 2 * 3")).toEqual(12);
-});
+describe("Runner long cases", () => {
+  it("20 + 1 * 10 - 5 * 3 - 3 **", () => {
+    expect(runner("20 + 1 * 10 - 5 * 3 - 3 **")).toEqual(6);
+  });
 
-test("runner: 2 * 2 + 3", () => {
-  expect(runner("2 * 2 + 3")).toEqual(7);
-});
-
-test("runner: 2 + 2 * 3", () => {
-  expect(runner("2 + 2 * 3")).toEqual(8);
-});
-
-test("runner: 20 + 1 * 10 - 5 * 3", () => {
-  expect(runner("20 + 1 * 10 - 5 * 3")).toEqual(15);
-});
-
-test("runner: 20 - 10 * 10 / 5 - 3", () => {
-  expect(runner("20 - 10 * 10 / 5 - 3")).toEqual(-3);
+  it("5 ^ 2 + 20 - 10 * 10 / 5 - 3 + ! 4", () => {
+    expect(runner("5 ^ 2 + 20 - 10 * 10 / 5 - 3 + ! 4")).toEqual(46);
+  });
 });
